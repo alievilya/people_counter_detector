80304648bb9a36b2ca8c16d779bd31c40b5353fe
Aliev Ilya <aliev.ilia@yandex.ru>
2020-11-22 03:09:08 +0300

    Update run_detection.py

--- run_detection.py
+++ run_detection.py
@@ -1,9 +1,8 @@
 import tensorflow as tf
-import sys
 from draw import select_object, draw_image
 import cv2
 from models import Yolov4
-from os.path import join
+
 
 def check_gpu():
     gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -21,38 +20,36 @@ def check_gpu():
             print(e)
 
 
-def bb_intersection_over_union(boxA, boxB):
+def bb_intersection_over_union(box_a, box_b):
     # determine the (x, y)-coordinates of the intersection rectangle
-    xA = max(boxA[0], boxB[0])
-    yA = max(boxA[1], boxB[1])
-    xB = min(boxA[2], boxB[2])
-    yB = min(boxA[3], boxB[3])
+    xa = max(box_a[0], box_b[0])
+    ya = max(box_a[1], box_b[1])
+    xb = min(box_a[2], box_b[2])
+    yb = min(box_a[3], box_b[3])
     # compute the area of intersection rectangle
-    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+    inter_area = max(0, xb - xa + 1) * max(0, yb - ya + 1)
     # compute the area of both the prediction and ground-truth
     # rectangles
-    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+    box_aArea = (box_a[2] - box_a[0] + 1) * (box_a[3] - box_a[1] + 1)
+    box_bArea = (box_b[2] - box_b[0] + 1) * (box_b[3] - box_b[1] + 1)
     # compute the intersection over union by taking the intersection
     # area and dividing it by the sum of prediction + ground-truth
     # areas - the interesection area
-    iou = interArea / float(boxAArea + boxBArea - interArea)
+    res_iou = inter_area / float(box_aArea + box_bArea - inter_area)
     # return the intersection over union value
-    return iou
+    return res_iou
 
 
 def go_outside(obj_coords, target_coords):
-
-    iou = bb_intersection_over_union(obj_coords, target_coords)
-    if iou >= 0.2:
+    res_iou = bb_intersection_over_union(obj_coords, target_coords)
+    if res_iou >= 0.2:
         return True
     else:
         return False
 
 
-def go_inside(prev_coords, target_coords):
-
-    iou1 = bb_intersection_over_union(prev_coords, target_coords)
+def go_inside(previous_coords, target_coords):
+    iou1 = bb_intersection_over_union(previous_coords, target_coords)
     if iou1 >= 0.4:
         return True
     else:
@@ -60,7 +57,7 @@ def go_inside(prev_coords, target_coords):
 
 
 def same_id(previous_id, current_id):
-    return previous_id==current_id
+    return previous_id == current_id
 
 
 if __name__ == "__main__":
@@ -124,7 +121,7 @@ if __name__ == "__main__":
                 counter_detection = num_frame
                 prev_coords = coordinates.copy()
 
-        if (num_frame - counter_detection > 5) and go_inside(prev_coords, door_array)\
+        if (num_frame - counter_detection > 5) and go_inside(prev_coords, door_array) \
                 and person_id not in person_inside.values():
             people_actions['in'] += 1
             person_inside['person_id'] = person_id
